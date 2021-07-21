afd34aa54d27478e180f18afed8a2cd131267520
babooolee <87624439+babooolee@users.noreply.github.com>
2021-07-21 12:49:16 +0900

    Add files via upload

--- main/portfolio.js
+++ main/portfolio.js
@@ -4,8 +4,14 @@ $(function(){
 $("#main").fullpage({
     anchors:['xm01', 'xm02', 'xm03', 'xm04', 'xm05'],
     afterLoad: function(origin, destination, direction){
+        //console.log(destination.index);
         $(".section").eq(destination.index).addClass("on").siblings().removeClass("on");
         $("nav li").eq(destination.index).addClass("on").siblings().removeClass("on");
+        if(destination.index===4) {
+           $("footer").css({color:"#fff"})
+        }else{
+           $("footer").removeAttr("style")
+        }
     },
 });
 
@@ -36,8 +42,34 @@ $(".pf01 .xi-play").on("click", function(){
     $(".p01_video").trigger("play");
 });
 
+$(".pf02 .xi-pause").on("click", function(){
+    $(".p02_video").trigger("pause");
+});
+
+$(".pf02 .xi-play").on("click", function(){
+    $(".p02_video").trigger("play");
+});
 
 
+$(".pt_slider").slick({
+    arrows:false,
+    slidesToShow: 3,
+    dots:true,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,          }
+        },
+      ]
+});
+
+$(".pf03 i.xi-arrow-left").on("click", function(){
+    $(".pt_slider").slick("slickPrev");
+});
+$(".pf03 i.xi-arrow-right").on("click", function(){
+    $(".pt_slider").slick("slickNext");
+});
 
 
 
